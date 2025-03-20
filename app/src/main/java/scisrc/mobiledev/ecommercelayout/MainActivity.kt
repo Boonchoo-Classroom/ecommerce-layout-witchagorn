eddfcce4e9491681eddd92a7414c16c26840aa41
witchagorn <witchagorn.n@ku.th>
2025-03-20 17:34:18 +0700

    witchagorn

--- app/src/main/java/scisrc/mobiledev/ecommercelayout/MainActivity.kt
+++ app/src/main/java/scisrc/mobiledev/ecommercelayout/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         // Setup toolbar
         setSupportActionBar(binding.toolbar)
 
